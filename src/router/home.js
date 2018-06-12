8943571afef2b4db7bab974ff8451699e0fe4e06
KYUUTA <200993697@qq.com>
2018-06-12 18:01:51 +0800

    fix bug..

--- src/router/home.js
+++ src/router/home.js
@@ -1,6 +1,6 @@
 const _import = require('./_import_' + process.env.NODE_ENV);
 
-const home = _import("home/Home");        // home
+const home = _import("home/home");        // home
 
 export default [
     {
